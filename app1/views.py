700452e86fb90ff6a15687ce9f595c7de8705224
Tu Usuario de GitHub <tu.email.github@ejemplo.com>
2026-09-15 00:26:53 -0300

    Fix: correccion de vistas en app1 y base de datos local

--- app1/views.py
+++ app1/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def vista_uno(request):
+    return HttpResponse("<h1>App 1 - Vista 1: Inicio</h1><p>Esta es la primera vista de la App 1.</p>")
+
+def vista_dos(request):
+    return HttpResponse("<h1>App 1 - Vista 2: Acerca de</h1><p>Esta es la segunda vista de la App 1.</p>")
